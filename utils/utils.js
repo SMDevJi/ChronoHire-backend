007473dd4716c0957e05e16ab41d2a7555be2f3b
SMDevJi <78262720+SMDevJi@users.noreply.github.com>
2026-04-14 21:58:29 +0530

    added option for setting ai model from env

--- utils/utils.js
+++ utils/utils.js
@@ -50,7 +50,7 @@ export async function generateAIQA(role, description, resumeText, difficulty, ye
     `;
 
         const result = await ai.models.generateContent({
-            model: "gemini-2.5-flash",
+            model: process.env.GEMINI_MODEL,
             contents: prompt,
         });
         const text = result.text;
@@ -160,7 +160,7 @@ candidate answer: ${q.transcript}
 
 
         const result = await ai.models.generateContent({
-            model: "gemini-2.5-flash",
+            model: process.env.GEMINI_MODEL,
             contents: prompt,
         });
         const responseText = result.text;
